efab540bdd581f5863dd80544bdcdd82b9993e54
TheFermiSea <squires.b@gmail.com>
2022-04-01 00:20:28 -0500

    added peak finding algo

--- sifparser/sifparser.py
+++ sifparser/sifparser.py
@@ -2,6 +2,8 @@
 import sif_reader
 from typing import List
 import xarray as xr 
+from scipy.signal import find_peaks
+import numpy as np
 
 def SifParser(filename: str,
               normalize=False) -> xr.DataArray.__getitem__:
@@ -20,6 +22,10 @@ def SifParser(filename: str,
     TODO: 
         add error checking
         add code to verify dimensionality matching for concatonation
+        peak finding algo
+        add con
+        
+        
     '''
     da = sif_reader.xr_open(filename)
     da = (da
@@ -27,16 +33,28 @@ def SifParser(filename: str,
             .set_index(width='Wavelength')
             .rename({'width':'Wavelength'})
             .drop_vars('calibration')
-    )/da.attrs['ExposureTime']
-    da['Wavelength'].attrs['units'] = 'nm'
-    da['Time'].attrs['units'] = 's'
-    da.name = 'Intensity'
-    da.attrs['units'] = 'counts/s'
-    da.attrs['filename'] = filename
+    )
+    if normalize==False:
+        da['Wavelength'].attrs['units'] = 'nm'
+        da['Time'].attrs['units'] = 's'
+        da.name = 'Intensity'
+        da.attrs['units'] = 'counts/s'
+    elif normalize==True:
+        da['Wavelength'].attrs['units'] = 'nm'
+        da['Time'].attrs['units'] = 's'
+        da.name = 'Intensity'
+        da.attrs['units'] = 'counts/s'
+        danorm = da/da.ExposureTime
+        danorm.attrs = da.attrs
+        da = danorm
+        del danorm
     return da
 
+
+
 def DataSetGenerator(filelist: List[str], 
-                     dimension='files')->xr.Dataset.__getitem__:
+                     dimensions='files',
+                    normalize=False)->xr.Dataset.__getitem__:
     '''
     Input:      
             filelist : .sif data file as string
@@ -44,21 +62,38 @@ def DataSetGenerator(filelist: List[str],
             
     Returns:    concatonated xr.Dataset object 
         
+    TODO 
+    add *args for dimension
     '''
     dataset = []
     for file in filelist:
-        da = SifParser(file)
+        da, danorm = SifParser(file)
         dataset.append(da)
-    ds = xr.concat(dataset, dim=dimension)
+    # ds = xr.concat(dataset, dim=dimension)
+    ds = xr.merge(dataset, compat='override')
     return ds
+
+def FindPeaks(da, numpeaks, width, time=0):
+    Prominence = 0
+    peaks, props = find_peaks(da.sel(Time=time).values[0], prominence=Prominence, width=width)
+    while len(peaks) > numpeaks:
+        Prominence += 1
+        peaks, props = find_peaks(da.sel(Time=time).values[0], prominence=Prominence, width=width)
+    da.attrs['PeakPixels'] = peaks
+    da.attrs['PeakProps'] = props
+    da.attrs['PeakWavelengths'] = da.Wavelength[peaks]
+    
+    return da 
+        
+    
+    
+
 
 
 #%%       Testbed
-from glob import glob 
 
-filelist = glob('*.sif')
-
-ds = DataSetGenerator(filelist)
+da = SifParser('/Users/briansquires/Documents/LIBS/data/20211026/1us4095mcp1.sif')
+da = FindPeaks(da, 2, 1)
     
         
     
