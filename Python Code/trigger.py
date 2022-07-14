fd3b5c2be7e6efef1f2c6fc4519de3f8bc1f08dd
TheFermiSea <squires.b@gmail.com>
2022-07-14 17:45:52 -0500

    trigger

--- Python Code/trigger.py
+++ Python Code/trigger.py
@@ -22,49 +22,11 @@ class Trigger(object):
             samps_per_chan = 1)
         
     def single_task(self):
-        tic = time.time()
         self.task.start()
         self.task.wait_until_done()
         self.task.stop()
-        toc = time.time()
-        print(toc - tic)
         
     def close(self):
         self.task.close()
         
-# Trigger = Trigger()
-# Trigger.configure()
-# print('initialized trigger')
-# for i in range(10):
-#     Trigger.single_task()
-# Trigger.close()
-
-
-# cam.SensorCooling = True
-
-# # getting legal available options for TemperatureControl feature
-# temps = cam.available_options_TemperatureControl
-# target_temp = temps[0] #if cam.TemperatureControl != temps[0] else temps[1]
-# print("Target temperature = {}C".format(target_temp))
-# # cam.TemperatureControl = target_temp
-
-# # waiting for temperature to stabilise
-# while(cam.TemperatureStatus != "Stabilised"):
-#     time.sleep(5)
-#     print("Temperature: {:.5f}C".format(cam.SensorTemperature), end="  ")
-#     print("Status: '{}'".format(cam.TemperatureStatus))
-#     if cam.TemperatureStatus == "Fault":
-#         err_str = "Camera faulted when cooling to target temperature"
-#         raise RuntimeError(err_str)
-
-# print("Sensor Temperature now Stabilised and Camera is ready to use")
-
-# cam.CycleMode = "Fixed"
-# cam.TriggerMode = 'External'
-# cam.ExposureTime = 0.1
-# print(cam.Delay)
-
-# acq = cam.acquire(timeout=20000)
-
-# acq.show()
 
